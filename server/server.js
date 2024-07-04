98b8aaedec24ee6c22aeb564ee2fe6b2e54ee5df
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-07-04 11:44:51 +0300

    server structure and connect to database

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("!!!");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Running Successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`.yellow.bold)
+);
